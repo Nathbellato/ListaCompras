1581449e8a2393461e85ba5160f04f29e6a21104
nath <natybellato@hotmail.com>
2018-06-26 20:48:21 -0300

    usando fragment

--- app/src/main/java/com/example/ncbel/listacompras/MainActivity.kt
+++ app/src/main/java/com/example/ncbel/listacompras/MainActivity.kt
@@ -4,26 +4,10 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.menu.*
 import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.valor_total)
-
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_itens)
-                return@OnNavigationItemSelectedListener true
-            }
-
-        }
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +19,5 @@ class MainActivity : AppCompatActivity() {
         lista_transacoes_listview.adapter = ListaTransacoesAdapter(transacoes, this)
 
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
